365ddc85e3b3baea031ed5abfc60aa0ccb57c783
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-15 19:55:55 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Четем от конзолата таксата за 1 година
+            int annualCharge = int.Parse(Console.ReadLine());
+            //2. Намираме цена за кецове = 60% от годишната такса 
+            double priceSneakers = annualCharge * 0.6;
+            //3. Намираме цена за екип = 80% от цената за кецове
+            double priceOutfit = priceSneakers * 0.8;
+            //4. Намираме цена за топка = цена за екип / 4 
+            double priceBall = priceOutfit / 4;
+            //5. Намираме цена за аксесоари = цена за топка / 5
+            double priceAccssories = priceBall / 5;
+            //6. Намираме общата сума = такса за година + кецове + екип + топка + аксесоари
+            double sum = annualCharge + priceSneakers + priceOutfit + priceBall + priceAccssories;
+            //7. Отпечатаме резулатата на конзолата ( общата сума)
+            Console.WriteLine(sum);
+        }
+    }
+}
