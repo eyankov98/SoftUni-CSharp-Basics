c3e0760736525dc493d6b97b5c57da33ba5a0f61
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-15 19:41:34 +0300

    Add files via upload

---
+++ First Steps In Coding - Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogPacks = int.Parse(Console.ReadLine());
+            double dogPacksPrice = dogPacks * 2.5;
+
+            int catPacks = int.Parse(Console.ReadLine());
+            double catPacksPrice = catPacks * 4;
+
+            double finalSum = dogPacksPrice + catPacksPrice;
+
+            Console.WriteLine($"{finalSum} lv.");
+        }
+    }
+}
