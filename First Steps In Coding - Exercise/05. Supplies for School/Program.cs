1ad2ea3bf817803120cfb0bdeba2b043d88f2543
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-15 19:54:14 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/05. Supplies for School/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05._Supplies_for_School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pens = int.Parse(Console.ReadLine());
+            int markers = int.Parse(Console.ReadLine());
+            int boardCleaner = int.Parse(Console.ReadLine());
+            int reduction = int.Parse(Console.ReadLine());
+
+            double pricePackPens = pens * 5.80;
+            double pricePackMarkers = markers * 7.20;
+            double priceBoardCleaner = boardCleaner * 1.20;
+            double finalReduction = reduction / 100.00;
+            double sum = pricePackPens + pricePackMarkers + priceBoardCleaner;
+            double priceReduction = sum - (sum * finalReduction);
+
+            Console.WriteLine(priceReduction);
+        }
+    }
+}
