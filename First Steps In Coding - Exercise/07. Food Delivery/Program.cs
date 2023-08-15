a1882efffb507a03727c9640aba9f96c0dd1274d
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-15 19:55:32 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/07. Food Delivery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int vegetarianMenus = int.Parse(Console.ReadLine());
+
+            double priceChikenMenus = chickenMenus * 10.35;
+            double priceFishMenus = fishMenus * 12.40;
+            double priceVegetarianMenus = vegetarianMenus * 8.15;
+            double priceMenus = priceChikenMenus + priceFishMenus + priceVegetarianMenus;
+            double priceDessertMenus = (20 / 100.00) * priceMenus;
+            double priceDelivery = 2.50;
+            double sum = priceMenus + priceDessertMenus + priceDelivery;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
