01333b843cd3a993b5622b59d40e004664cecce8
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-15 19:52:57 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Четем данните от депозита
+            //-  Депозирана сума – double
+            //-  Срок на депозита (в месеци) - int
+            //-  Годишен лихвен процент - double
+            double deposit = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine());
+            //2. Изчисляваме сумата:
+            // сума = депозит + срок * ((депозит * годишен лихвен процент) / 12)
+            double sum = deposit + term * ((deposit * rate / 100) / 12);
+            //3. Отпечатваме резултата на конзолата (сума)
+            Console.WriteLine(sum);
+        }
+    }
+}
