6d6251c710a951c0a0b3327480fe6eb76fa6904e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-15 19:56:49 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volumeAquarium = length * width * height;
+            double volumeLiters = volumeAquarium * 0.001;
+            double occupiedSpace = percent / 100.00;
+            double litersNeeded = volumeLiters * (1 - occupiedSpace);
+
+            Console.WriteLine(litersNeeded);
+        }
+    }
+}
