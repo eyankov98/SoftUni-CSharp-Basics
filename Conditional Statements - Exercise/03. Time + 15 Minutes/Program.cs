801165f51088515222c5e206645da06790c2920c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-16 19:51:43 +0300

    Add files via upload

---
+++ Conditional Statements - Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 15;
+            if (minutes >= 60)
+            {
+                minutes = minutes - 60;
+                hours = hours + 1;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
