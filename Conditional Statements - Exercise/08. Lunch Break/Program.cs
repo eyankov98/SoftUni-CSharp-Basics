ba133800e962a6f973fd8dc4cc641a1943f3f825
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-16 19:55:35 +0300

    Add files via upload

---
+++ Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Четем от конзолата: име на сериал, време на епизод, обедна почивка
+            string seriesName = Console.ReadLine();
+            int seriesTime = int.Parse(Console.ReadLine());
+            int lunchTime = int.Parse(Console.ReadLine());
+            // Изчисляваме колко време от почивката оста за сериал
+            double timeForSeries = lunchTime * 5 / 8.00;
+            //Проверяваме дали времето е достатъчно
+            if (timeForSeries >= seriesTime)
+            //=> достатъчно е(Резултата трябва да се форматира нагоре)
+            {
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeForSeries - seriesTime)} minutes free time.");
+            }
+            else
+            //=> Не е достатъчно(Резултата трябва да се форматира нагоре)
+            {
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(seriesTime - timeForSeries)} more minutes.");
+            }
+        }
+    }
+}
