11ebe598d04775a0fa8de4abe8bca83535c7f91f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-16 19:43:56 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Excellent_Result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input(ВХОД)
+            double grade = double.Parse(Console.ReadLine());
+            //Act(ВЪРШИ ДЕЙСТВИЯ)
+            if (grade >= 5.50)
+            {
+                //Outpu(ИЗХОД)
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
