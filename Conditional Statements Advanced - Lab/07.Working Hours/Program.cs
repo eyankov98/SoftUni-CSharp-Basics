178fbbf6a528d74f95808553bb130882eb1f670c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-17 12:20:30 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/07.Working Hours/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int partOfDay = int.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday" || dayOfWeek == "Saturday")
+            {
+                if (partOfDay >= 10 && partOfDay < 18)
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+            if (dayOfWeek == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
