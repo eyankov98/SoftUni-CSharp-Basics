65f97bdf2c8bdc85c6da11d11d5772fe07523d8c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-18 15:57:53 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double totalIncome = 0;
+
+            if (projectionType == "Premiere")
+            {
+                totalIncome = rows * columns * 12;
+            }
+            else if (projectionType == "Normal")
+            {
+                totalIncome = rows * columns * 7.50;
+            }
+            else if (projectionType == "Discount")
+            {
+                totalIncome = rows * columns * 5.00;
+            }
+            Console.WriteLine($"{totalIncome:f2} leva");
+        }
+    }
+}
