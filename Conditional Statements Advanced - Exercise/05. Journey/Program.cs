952f1aa2523c8e3108c649d02085a8a7e759a21c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-18 16:00:55 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Exercise/05. Journey/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _05._Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double moneySpent = 0;
+            string typeVacation = ""; 
+            string destination = ""; 
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    typeVacation = "Camp";
+                    moneySpent = budget * 0.30;
+                }
+                else if (season == "winter")
+                {
+                    typeVacation = "Hotel";
+                    moneySpent = budget * 0.70;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    typeVacation = "Camp";
+                    moneySpent = budget * 0.40;
+                }
+                else if (season == "winter")
+                {
+                    typeVacation = "Hotel";
+                    moneySpent = budget * 0.80;
+                }
+            }
+            else
+            {
+                destination = "Europe";
+                if (season == "summer" || season == "winter")
+                {
+                    typeVacation = "Hotel";
+                    moneySpent = budget * 0.90;
+                }
+            }
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{typeVacation} - {moneySpent:f2}");
+        }
+    }
+}
