3a660e983a0b265f86ed2637c75f6be5e3726eb0
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-18 16:03:50 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Exercise/08. On Time for the Exam/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _08._On_Time_for_the_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMinutes = int.Parse(Console.ReadLine());
+            int arrivingHour = int.Parse(Console.ReadLine());
+            int arrivingMinutes = int.Parse(Console.ReadLine());
+
+            int examTimeInMinutes = examHour * 60 + examMinutes;
+            int arrivingTimeInMinutes = arrivingHour * 60 + arrivingMinutes;
+            int timeDiference = Math.Abs(examTimeInMinutes - arrivingTimeInMinutes);
+            string statment = "";
+            int hourConver = 0;
+            int minutesConvert = 0;
+
+            if (examTimeInMinutes < arrivingTimeInMinutes)
+            {
+                statment = "Late";
+                if (timeDiference >= 1 && timeDiference <= 59)
+                {
+                    Console.WriteLine(statment);
+                    Console.WriteLine($"{timeDiference} minutes after the start");
+                }
+                else
+                {
+                    hourConver = timeDiference / 60;
+                    minutesConvert = timeDiference % 60;
+                    Console.WriteLine(statment);
+                    Console.WriteLine($"{hourConver}:{minutesConvert:d2} hours after the start");
+                }
+            }
+            else if (examTimeInMinutes >= arrivingTimeInMinutes && timeDiference <= 30)
+            {
+                statment = "On time";
+
+                if (timeDiference > 0 && timeDiference <= 30)
+                {
+                    Console.WriteLine(statment);
+                    Console.WriteLine($"{timeDiference} minutes before the start");
+                }
+                else
+                {
+                    Console.WriteLine(statment);
+                }
+            }
+            else if (examTimeInMinutes > arrivingTimeInMinutes && timeDiference > 30)
+            {
+                statment = "Early";
+                if (timeDiference <= 59)
+                {
+                    Console.WriteLine(statment);
+                    Console.WriteLine($"{timeDiference} minutes before the start");
+                }
+                else
+                {
+                    hourConver = timeDiference / 60;
+                    minutesConvert = timeDiference % 60;
+                    Console.WriteLine(statment);
+                    Console.WriteLine($"{hourConver}:{minutesConvert:d2} hours before the start");
+                }
+            }
+        }
+    }
+}
