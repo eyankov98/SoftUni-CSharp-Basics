3c4d942905f3a78ab3dd138c262d503fc2699c6a
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-19 12:43:32 +0300

    Add files via upload

---
+++ For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                sum1 = sum1 + number;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                sum2 = sum2 + number;
+            }
+            if (sum1 == sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum1}");
+            }
+            else
+            {
+                int diff = Math.Abs(sum1 - sum2);
+                Console.WriteLine($"No, diff = " + diff);
+            }
+        }
+    }
+}
