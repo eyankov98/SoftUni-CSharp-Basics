35b7d3313f00554a556239c5545f6a674692ec02
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-20 22:00:10 +0300

    Add files via upload

---
+++ For Loop - Exercise/03. Histogram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _03._Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 200)
+                {
+                    p1++;
+                }
+                else if (number >= 200 && number <= 399)
+                {
+                    p2++;
+                }
+                else if (number >= 400 && number <= 599)
+                {
+                    p3++;
+                }
+                else if (number >= 600 && number <= 799)
+                {
+                    p4++;
+                }
+                else if (number >= 800)
+                {
+                    p5++;
+                }
+            }
+            p1 = p1 / n * 100;
+            p2 = p2 / n * 100;
+            p3 = p3 / n * 100;
+            p4 = p4 / n * 100;
+            p5 = p5 / n * 100;
+
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+            Console.WriteLine($"{p4:f2}%");
+            Console.WriteLine($"{p5:f2}%");
+        }
+    }
+}
