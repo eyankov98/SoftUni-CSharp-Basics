2b3122fddd67ec1bfb560e786906c30119491c47
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-20 22:02:54 +0300

    Add files via upload

---
+++ For Loop - Exercise/06. Oscars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfActor = Console.ReadLine();
+            double pointsOfAcademy = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string nameOfAssessor = Console.ReadLine();
+                double pointsOfAssessor = double.Parse(Console.ReadLine());
+
+                pointsOfAcademy = pointsOfAcademy + ((nameOfAssessor.Length * pointsOfAssessor) / 2);
+
+                if (pointsOfAcademy > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {nameOfActor} got a nominee for leading role with {pointsOfAcademy:f1}!");
+                    break;
+                }
+            }
+
+            if (pointsOfAcademy < 1250.5)
+            {
+                Console.WriteLine($"Sorry, {nameOfActor} you need {1250.5 - pointsOfAcademy:f1} more!");
+            }
+        }
+    }
+}
