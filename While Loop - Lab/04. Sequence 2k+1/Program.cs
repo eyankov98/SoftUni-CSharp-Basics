c9b0ce9940ad467c180b352b9e0d81e3ca6495e7
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-21 23:27:16 +0300

    Add files via upload

---
+++ While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Sequence_2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int numbers = 1;
+
+            while (numbers <= n)
+            {
+                Console.WriteLine(numbers);
+
+                numbers = numbers * 2 + 1;
+            }
+        }
+    }
+}
