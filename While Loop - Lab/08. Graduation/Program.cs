b7a7c31505ccbe781d1db1563e8918f6a42dcccc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-21 23:30:12 +0300

    Add files via upload

---
+++ While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int schoolClass = 1;
+
+            double sumGrades = 0;
+
+            int excludCount = 0;
+
+            while (schoolClass <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade < 4)
+                {
+                    excludCount++;
+
+                    if (excludCount == 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {schoolClass} grade");
+                        return;
+                    }
+                }
+                else
+                {
+                    schoolClass++;
+                }
+                sumGrades = sumGrades + grade;
+            }
+            double avarageGrade = sumGrades / 12;
+
+            Console.WriteLine($"{name} graduated. Average grade: {avarageGrade:f2}");
+        }
+    }
+}
