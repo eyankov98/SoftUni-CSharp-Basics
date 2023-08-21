fb34fa4182cefd9513514566f1af633dc4826f9e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-21 23:27:44 +0300

    Add files via upload

---
+++ While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double totalMoney = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double money = double.Parse(input);
+
+                if (money < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                Console.WriteLine($"Increase: {money:f2}");
+
+                totalMoney = totalMoney + money;
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {totalMoney:f2}");
+        }
+    }
+}
