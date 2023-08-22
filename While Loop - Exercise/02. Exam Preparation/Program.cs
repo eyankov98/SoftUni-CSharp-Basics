c9dba9017a213e704ff4f7b51398295d9c0da7d9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:04:10 +0300

    Add files via upload

---
+++ While Loop - Exercise/02. Exam Preparation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02._Exam_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countUnsatisfactoryGrades = int.Parse(Console.ReadLine());
+            string nameExercise = Console.ReadLine();
+
+            int countBadGrades = 0;
+            string lastNameExercise = "";
+            int countExercise = 0;
+            int sumGrades = 0;
+
+
+            while (nameExercise != "Enough")
+            {
+                int grade = int.Parse(Console.ReadLine());
+
+                if (grade <= 4)
+                {
+                    countBadGrades++;
+
+                    if (countBadGrades == countUnsatisfactoryGrades)
+                    {
+                        Console.WriteLine($"You need a break, {countBadGrades} poor grades.");
+                        return;
+                    }
+                }
+
+                sumGrades = sumGrades + grade;
+                countExercise++;
+                lastNameExercise = nameExercise;
+
+                nameExercise = Console.ReadLine();
+            }
+
+            double avarageGrade = (double)sumGrades / countExercise;
+
+            Console.WriteLine($"Average score: {avarageGrade:f2}");
+            Console.WriteLine($"Number of problems: {countExercise}");
+            Console.WriteLine($"Last problem: {lastNameExercise}");
+        }
+    }
+}
