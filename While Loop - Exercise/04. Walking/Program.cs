ecbc85417539c3edcec2bd99caa3669f7c8fd6c7
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:05:28 +0300

    Add files via upload

---
+++ While Loop - Exercise/04. Walking/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "Going home")
+            {
+                int steps = int.Parse(input);
+
+                goal = goal + steps;
+
+                if (goal >= 10000)
+                {
+                    goal = goal - 10000;
+
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{goal} steps over the goal!");
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+            int stepsToHome = int.Parse(Console.ReadLine());
+            goal = goal + stepsToHome;
+
+            if (goal >= 10000)
+            {
+                goal = goal - 10000;
+
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{goal} steps over the goal!");
+            }
+            else
+            {
+                goal = 10000 - goal;
+                Console.WriteLine($"{goal} more steps to reach goal.");
+            }
+        }
+    }
+}
