077c3671faad6635880bc8db02b37663c28049da
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:08:21 +0300

    Add files via upload

---
+++ While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            int cakeSize = width * length;
+
+            while (command != "STOP")
+            {
+                int piecesTaken = int.Parse(command);
+
+                cakeSize = cakeSize - piecesTaken;
+
+                if (cakeSize <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {cakeSize * -1} pieces more.");
+                    return;
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{cakeSize} pieces are left.");
+        }
+    }
+}
