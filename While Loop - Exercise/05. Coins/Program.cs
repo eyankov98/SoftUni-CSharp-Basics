c2c730f6f6630bb7f12d0f5360d13ee53a10792f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:08:04 +0300

    Add files via upload

---
+++ While Loop - Exercise/05. Coins/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _05._Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double coins = double.Parse(Console.ReadLine()) * 100;
+
+            int coinsCount = 0;
+
+            while (coins >= 200)
+            {
+                coins = coins - 200;
+                coinsCount++;
+            }
+
+            while (coins >= 100)
+            {
+                coins = coins - 100;
+                coinsCount++;
+            }
+
+            while (coins >= 50)
+            {
+                coins = coins - 50;
+                coinsCount++;
+            }
+
+            while (coins >= 20)
+            {
+                coins = coins - 20;
+                coinsCount++;
+            }
+            while (coins >= 10)
+            {
+                coins = coins - 10;
+                coinsCount++;
+            }
+
+            while (coins >= 5)
+            {
+                coins = coins - 5;
+                coinsCount++;
+            }
+
+            while (coins >= 2)
+            {
+                coins = coins - 2;
+                coinsCount++;
+            }
+
+            while (coins >= 1)
+            {
+                coins = coins - 1;
+                coinsCount++;
+            }
+
+            Console.WriteLine(coinsCount);
+        }
+    }
+}
