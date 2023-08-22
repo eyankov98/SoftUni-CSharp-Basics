daf5a48f2c3dd1ecb092a153f75375a1cff81a8e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:09:00 +0300

    Add files via upload

---
+++ While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            int freeSpace = width * length * height;
+
+            int totalCubicMeters = 0;
+
+            while (command != "Done")
+            {
+                int cubicMeters = int.Parse(command);
+
+                totalCubicMeters = totalCubicMeters + cubicMeters;
+
+                if (totalCubicMeters > freeSpace)
+                {
+                    Console.WriteLine($"No more free space! You need {totalCubicMeters - freeSpace} Cubic meters more.");
+                    return;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (freeSpace > totalCubicMeters)
+            {
+                Console.WriteLine($"{freeSpace - totalCubicMeters} Cubic meters left.");
+            }
+        }
+    }
+}
