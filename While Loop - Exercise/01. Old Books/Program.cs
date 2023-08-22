91efa9da00105160909275aa7b3b38d608f61dbe
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:05:52 +0300

    Add files via upload

---
+++ While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookAnnie = Console.ReadLine();
+            string book = Console.ReadLine();
+
+            int countBooks = 0;
+
+            while (book != "No More Books")
+            {
+                if (book == bookAnnie)
+                {
+                    Console.WriteLine($"You checked {countBooks} books and found it.");
+                    return;
+                }
+
+                countBooks++;
+
+                book = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The book you search is not here!");
+            Console.WriteLine($"You checked {countBooks} books.");
+        }
+    }
+}
