bbd230956466cdeb006f7ed5999d0c77e85894c4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-22 23:04:42 +0300

    Add files via upload

---
+++ While Loop - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyNeeded = double.Parse(Console.ReadLine());
+            double moneyAvailable = double.Parse(Console.ReadLine());
+
+            int countSpendDays = 0;
+
+            int countDays = 0;
+
+            while (moneyNeeded > moneyAvailable)
+            {
+                string command = Console.ReadLine();
+
+                double sumSpendOrSave = double.Parse(Console.ReadLine());
+
+                countDays++;
+
+                if (command == "spend")
+                {
+                    countSpendDays++;
+
+                    if (countSpendDays == 5)
+                    {
+                        Console.WriteLine("You can't save the money.");
+                        Console.WriteLine(countDays);
+                        return;
+                    }
+
+                    moneyAvailable = moneyAvailable - sumSpendOrSave;
+
+                    if (moneyAvailable <= 0)
+                    {
+                        moneyAvailable = 0;
+                    }
+                }
+                else if (command == "save")
+                {
+                    countSpendDays = 0;
+                    moneyAvailable = moneyAvailable + sumSpendOrSave;
+                }
+            }
+            Console.WriteLine($"You saved the money for {countDays} days.");
+        }
+    }
+}
