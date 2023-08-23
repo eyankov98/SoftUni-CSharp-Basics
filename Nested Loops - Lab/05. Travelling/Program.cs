a9668fac839fb05d483166b9ae0dc0090d7aead6
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-23 22:44:40 +0300

    Add files via upload

---
+++ Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            double budgetNeeded;
+            double savedMoney = 0;
+            double money = 0;
+
+            while (destination != "End")
+            {
+                budgetNeeded = double.Parse(Console.ReadLine());
+                savedMoney = 0;
+                while (budgetNeeded > savedMoney)
+                {
+                    money = double.Parse(Console.ReadLine());
+                    savedMoney += money;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
