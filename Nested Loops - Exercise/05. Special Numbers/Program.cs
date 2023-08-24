72e5ecef3f2a1beeb9991be3711292761a9c4170
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-24 21:11:54 +0300

    Add files via upload

---
+++ Nested Loops - Exercise/05. Special Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                bool isSpecial = true;
+                int number = i;
+
+                for (int j = 1; j <= 4; j++)
+                {
+                    int digit = number % 10;
+                    number = number / 10;
+
+                    if (digit == 0)
+                    {
+                        isSpecial = false;
+                    }
+                    else if (n % digit != 0)
+                    {
+                        isSpecial = false;
+                    }
+                    else if (n < digit)
+                    {
+                        isSpecial = false;
+                    }
+                }
+
+                if (isSpecial == true)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
