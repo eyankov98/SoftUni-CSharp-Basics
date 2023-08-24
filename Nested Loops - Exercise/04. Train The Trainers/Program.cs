30b439a260ef4154d0d9cc2cd194e89476c8378f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-24 21:09:41 +0300

    Add files via upload

---
+++ Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int jury = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            double grade = 0;
+
+            double sumTotalGrades = 0;
+            int exercises = 0;
+
+
+            while (command != "Finish")
+            {
+                double sumGrades = 0;
+
+                for (int i = 1; i <= jury; i++)
+                {
+                    grade = double.Parse(Console.ReadLine());
+                    sumGrades = sumGrades + grade;
+                }
+
+                double averageGrade = sumGrades / jury;
+                Console.WriteLine($"{command} - {averageGrade:f2}.");
+
+                sumTotalGrades = sumTotalGrades + averageGrade;
+                exercises++;
+
+                command = Console.ReadLine();
+            }
+
+            double averageTotalGrade = sumTotalGrades / exercises;
+
+            Console.WriteLine($"Student's final assessment is {averageTotalGrade:f2}.");
+        }
+    }
+}
