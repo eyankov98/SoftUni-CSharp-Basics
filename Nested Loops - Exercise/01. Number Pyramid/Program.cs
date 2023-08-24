e722831f90738c76e874d85ff0167f7d1edab1c9
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-24 21:08:29 +0300

    Add files via upload

---
+++ Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int columns = 1; columns <= row; columns++)
+                {
+                    Console.Write($"{currentNumber} ");
+                    currentNumber++;
+                    if (currentNumber > n)
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
