2546f6c1ff18b1d00d750b21ed5e6fdbfa341a5f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-25 19:23:22 +0300

    Add files via upload

---
+++ Programming Basics Exam - 9 March 2019/06. High Jump/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06._High_Jump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wantedHeight = int.Parse(Console.ReadLine());
+            int startingHeight = wantedHeight - 30;
+
+            int jumpHeight = 0;
+            int fails = 0;
+            int jumps = 0;
+
+            for (int currentHeight = startingHeight; currentHeight <= wantedHeight; currentHeight += 5)
+            {
+                for (int tryNumber = 1; tryNumber <= 3; tryNumber++)
+                {
+                    jumpHeight = int.Parse(Console.ReadLine());
+
+                    if (jumpHeight > currentHeight)
+                    {
+                        jumps++;
+                        tryNumber = 3;
+                    }
+
+                    else if (jumpHeight <= currentHeight)
+                    {
+                        fails++;
+                        jumps++;
+
+                        if (fails == 3)
+                        {
+                            Console.WriteLine($"Tihomir failed at {currentHeight}cm after {jumps} jumps.");
+                            return;
+                        }
+                    }
+                }
+
+                fails = 0;
+            }
+
+            Console.WriteLine($"Tihomir succeeded, he jumped over {wantedHeight}cm after {jumps} jumps.");
+        }
+    }
+}
