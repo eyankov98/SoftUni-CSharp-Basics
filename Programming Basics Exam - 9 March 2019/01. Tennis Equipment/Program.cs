c56434b73be5ba6b7ad36103738c9e12f883dac8
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-25 19:19:05 +0300

    Add files via upload

---
+++ Programming Basics Exam - 9 March 2019/01. Tennis Equipment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Tennis_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tennisRacketPrice = double.Parse(Console.ReadLine());
+            int tennisRacketCount = int.Parse(Console.ReadLine());
+            int shoePacksCount = int.Parse(Console.ReadLine());
+
+            double shoePackPrice = tennisRacketPrice / 6;
+            double firstBill = (shoePackPrice * shoePacksCount) + (tennisRacketPrice * tennisRacketCount);
+            double otherStuff = firstBill * 0.2;
+            double secondBill = firstBill + otherStuff;
+
+            double playerBill = secondBill / 8;
+            double sponsorsBill = secondBill - playerBill;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(playerBill)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(sponsorsBill)}");
+        }
+    }
+}
