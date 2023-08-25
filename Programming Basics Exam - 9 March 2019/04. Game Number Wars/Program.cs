ca89450e9f7fbbfbfd61749d038eae3d63a910de
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-25 19:22:28 +0300

    Add files via upload

---
+++ Programming Basics Exam - 9 March 2019/04. Game Number Wars/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _04._Game_Number_Wars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstPlayerName = Console.ReadLine();
+            string secondPlayerName = Console.ReadLine();
+
+            string command = string.Empty;
+
+            int firstPlayerCardNumber = 0;
+            int secondPlayerCardNumber = 0;
+
+            int firstPlayerPoints = 0;
+            int secondPlayerPoints = 0;
+
+            int firstPlayerPointsSum = 0;
+            int secondPlayerPointsSum = 0;
+
+            while ((command = Console.ReadLine()) != "End of game")
+            {
+                firstPlayerCardNumber = int.Parse(command);
+                secondPlayerCardNumber = int.Parse(Console.ReadLine());
+
+                if (firstPlayerCardNumber > secondPlayerCardNumber)
+                {
+                    firstPlayerPoints = firstPlayerCardNumber - secondPlayerCardNumber;
+                    firstPlayerPointsSum += firstPlayerPoints;
+                }
+
+                else if (firstPlayerCardNumber < secondPlayerCardNumber)
+                {
+                    secondPlayerPoints = secondPlayerCardNumber - firstPlayerCardNumber;
+                    secondPlayerPointsSum += secondPlayerPoints;
+                }
+
+                else if (firstPlayerCardNumber == secondPlayerCardNumber)
+                {
+                    Console.WriteLine("Number wars!");
+                    firstPlayerCardNumber = int.Parse(Console.ReadLine());
+                    secondPlayerCardNumber = int.Parse(Console.ReadLine());
+
+                    if (firstPlayerCardNumber > secondPlayerCardNumber)
+                    {
+                        firstPlayerPoints = firstPlayerCardNumber - secondPlayerCardNumber;
+                        Console.WriteLine($"{firstPlayerName} is winner with {firstPlayerPoints} points");
+                        return;
+                    }
+                    else if (firstPlayerCardNumber < secondPlayerCardNumber)
+                    {
+                        secondPlayerPoints = secondPlayerCardNumber - firstPlayerCardNumber;
+                        Console.WriteLine($"{secondPlayerName} is winner with {secondPlayerPoints} points");
+                        return;
+                    }
+                }
+
+                firstPlayerPoints = 0;
+                secondPlayerPoints = 0;
+            }
+
+            Console.WriteLine($"{firstPlayerName} has {firstPlayerPointsSum} points");
+            Console.WriteLine($"{secondPlayerName} has {secondPlayerPointsSum} points");
+        }
+    }
+}
