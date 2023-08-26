27c8476545aa8a725f71d0fe05034c96ca5f4aaa
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-26 21:02:39 +0300

    Add files via upload

---
+++ Programming Basics Exam - 10 March 2019/03. World Snooker Championship/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _03._World_Snooker_Championship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string partOfChampionship = Console.ReadLine();
+            string typeTicket = Console.ReadLine();
+            int numberOfTickets = int.Parse(Console.ReadLine());
+            char photo = char.Parse(Console.ReadLine());
+            double ticketPrice = 0;
+            double photoWithTrophy = 40;
+
+            if (partOfChampionship == "Quarter final")
+            {
+                if (typeTicket == "Standard")
+                {
+                    ticketPrice = 55.50;
+                }
+                else if (typeTicket == "Premium")
+                {
+                    ticketPrice = 105.20;
+                }
+                else if (typeTicket == "VIP")
+                {
+                    ticketPrice = 118.90;
+                }
+            }
+            else if (partOfChampionship == "Semi final")
+            {
+                if (typeTicket == "Standard")
+                {
+                    ticketPrice = 75.88;
+                }
+                else if (typeTicket == "Premium")
+                {
+                    ticketPrice = 125.22;
+                }
+                else if (typeTicket == "VIP")
+                {
+                    ticketPrice = 300.40;
+                }
+            }
+            else if (partOfChampionship == "Final")
+            {
+                if (typeTicket == "Standard")
+                {
+                    ticketPrice = 110.10;
+                }
+                else if (typeTicket == "Premium")
+                {
+                    ticketPrice = 160.66;
+                }
+                else if (typeTicket == "VIP")
+                {
+                    ticketPrice = 400;
+                }
+            }
+            double totalMoney = ticketPrice * numberOfTickets;
+            if (totalMoney > 4000)
+            {
+                totalMoney = totalMoney * 0.75;
+                photo = 'N';
+            }
+            else if (totalMoney > 2500)
+            {
+                totalMoney = totalMoney * 0.9;
+            }
+            if (photo == 'Y')
+            {
+                totalMoney = totalMoney + numberOfTickets * 40;
+            }
+            Console.WriteLine($"{totalMoney:f2}");
+        }
+    }
+}
