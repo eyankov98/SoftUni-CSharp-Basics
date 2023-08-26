e5e54f91e65fec5a6c470e62f171edde154a0970
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-26 21:01:13 +0300

    Add files via upload

---
+++ Programming Basics Exam - 10 March 2019/01. Basketball Equipment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearTax = int.Parse(Console.ReadLine());
+
+            double shoes = yearTax - ((double)yearTax * 0.4);
+            double set = shoes - (shoes * 0.2);
+            double ball = set / 4;
+            double accessories = ball / 5;
+            double yearTaxSum = yearTax + shoes + set + ball + accessories;
+
+            Console.WriteLine($"{yearTaxSum:f2}");
+        }
+    }
+}
