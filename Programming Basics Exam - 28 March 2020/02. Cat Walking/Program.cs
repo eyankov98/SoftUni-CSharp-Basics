f16257ef6228e3969ca23382a5edf11e9f131b2f
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-27 23:10:37 +0300

    Add files via upload

---
+++ Programming Basics Exam - 28 March 2020/02. Cat Walking/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Cat_Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesWalking = int.Parse(Console.ReadLine());
+            int countWalking = int.Parse(Console.ReadLine());
+            int callories = int.Parse(Console.ReadLine());
+
+            long allMinutesWalking = countWalking * minutesWalking;
+            long allCalloriesBurn = allMinutesWalking * 5;
+
+            if (allCalloriesBurn >= (callories * 0.5))
+            {
+                Console.WriteLine("Yes, the walk for your cat is enough. Burned calories per day: {0}.", allCalloriesBurn);
+            }
+            else if (allCalloriesBurn < (callories * 0.5))
+            {
+                Console.WriteLine("No, the walk for your cat is not enough. Burned calories per day: {0}.", allCalloriesBurn);
+            }
+        }
+    }
+}
