07965f2c2b84c639e450a20abe49e66651667ea5
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-27 23:11:38 +0300

    Add files via upload

---
+++ Programming Basics Exam - 28 March 2020/05. Care of Puppy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Care_of_Puppy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfFood = int.Parse(Console.ReadLine());
+            amountOfFood = amountOfFood * 1000;
+
+            int foodNeeded = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Adopted")
+            {
+                int food = int.Parse(command);
+
+                foodNeeded = foodNeeded + food;
+
+                command = Console.ReadLine();
+            }
+
+            if (foodNeeded <= amountOfFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {amountOfFood - foodNeeded} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {foodNeeded - amountOfFood} grams more.");
+            }
+        }
+    }
+}
