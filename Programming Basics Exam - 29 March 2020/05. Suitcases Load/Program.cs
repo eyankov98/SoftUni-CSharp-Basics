cfae820fcef260a34f2613a87331a22f9503d4d1
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-28 20:19:22 +0300

    Add files via upload

---
+++ Programming Basics Exam - 29 March 2020/05. Suitcases Load/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _05._Suitcases_Load
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double capacityOfLuggagePlace = double.Parse(Console.ReadLine());
+            string input = string.Empty;
+            int suitcase = 0;
+            int loadedSuitcase = 0;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                suitcase++;
+                double volumeOfLuggage = double.Parse(input);
+
+                if ((suitcase % 3 == 0) && (capacityOfLuggagePlace < volumeOfLuggage * 1.1))
+                {
+                    break;
+                }
+
+                if ((suitcase % 3 != 0) && (capacityOfLuggagePlace < volumeOfLuggage))
+                {
+                    break;
+                }
+
+                if (suitcase % 3 == 0)
+                {
+                    capacityOfLuggagePlace -= volumeOfLuggage * 0.1;
+                }
+
+                capacityOfLuggagePlace -= volumeOfLuggage;
+                loadedSuitcase++;
+            }
+
+            if (input == "End")
+            {
+                Console.WriteLine($"Congratulations! All suitcases are loaded!");
+            }
+            else
+            {
+                Console.WriteLine("No more space!");
+            }
+
+            Console.WriteLine($"Statistic: {loadedSuitcase} suitcases loaded.");
+        }
+    }
+}
