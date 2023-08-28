29ff46c38324fde5db3158f4428ac7de6e10d796
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2023-08-28 20:18:04 +0300

    Add files via upload

---
+++ Programming Basics Exam - 29 March 2020/02. Mountain Run/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Mountain_Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double timeForClimbing = distanceInMeters * timeForOneMeter;
+            double delaySeconds = (Math.Floor(distanceInMeters / 50)) * 30;
+            double finalClimbingInSeconds = timeForClimbing + delaySeconds;
+
+            if (recordInSeconds <= finalClimbingInSeconds)
+            {
+                Console.WriteLine($"No! He was {(finalClimbingInSeconds - recordInSeconds):f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! The new record is {finalClimbingInSeconds:f2} seconds.");
+            }
+        }
+    }
+}
